44947da57cdd88878f04072e6d216f8c96991f0c
aixion1506 <aixion1506@gmail.com>
2024-08-10 22:56:12 +0900

    Map & Filter 함수

---
+++ 27-map/src/App.js
@@ -0,0 +1,16 @@
+import Alphabet from './Alphabet';
+import './App.css';
+import Base from './Base';
+
+function App() {
+  return (
+    <>
+      <Base />
+      <hr />
+
+      <Alphabet />
+    </>
+  );
+}
+
+export default App;
