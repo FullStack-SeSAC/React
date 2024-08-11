b553dceed5e2a75a7d166cb29d1a33779448ecae
aixion1506 <aixion1506@gmail.com>
2024-08-12 01:00:51 +0900

    React-LifeCycle

--- 28-lifecycle/src/App.js
+++ 28-lifecycle/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import LifeCycleClass from './component/LifeCycleClass';
+import LifeCycleFunc from './component/LifeCycleFunc';
+import LifeCycleFuncChild from './component/LifeCycleFuncChild';
+import Ref1 from './component/Ref1';
+import Ref2 from './component/Ref2';
+import Ref3 from './component/Ref3';
+import Ref4 from './component/Ref4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Ref1 />
+      <hr />
+
+      <Ref2 />
+      <hr />
+
+      <Ref3 />
+      <hr />
+
+      <Ref4 />
+      <hr />
+
+      {/* <LifeCycleClass /> */}
+
+      <LifeCycleFunc />
+
+      <LifeCycleFuncChild />
+
+    </>
   );
 }
 
